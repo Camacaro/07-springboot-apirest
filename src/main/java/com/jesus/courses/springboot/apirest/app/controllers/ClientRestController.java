e8d8486f5bc81dc07dc0ffee15f328feac3eb734
Jesus Camacaro <jesus.alejandr@gmail.com>
2023-11-28 08:16:43 -0300

    cors added

--- src/main/java/com/jesus/courses/springboot/apirest/app/controllers/ClientRestController.java
+++ src/main/java/com/jesus/courses/springboot/apirest/app/controllers/ClientRestController.java
@@ -4,12 +4,14 @@ import com.jesus.courses.springboot.apirest.app.models.entity.Client;
 import com.jesus.courses.springboot.apirest.app.models.services.IClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/client")
 public class ClientRestController {
